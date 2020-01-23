b454238a8d23b4d0daa284a5c46c6c38d9b09227
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-01-22 22:07:37 -0500

    adf

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use('/users', usersRouter);
  * Experimenting with launching on Heroku
  */
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static("../build"))
+    app.use(express.static("build"))
 
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "build", "index.html"));
